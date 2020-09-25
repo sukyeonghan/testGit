314108b13c3972ab99179136131e5c529a2bc235
gkstn <gkstn@DESKTOP-BKF6HH6>
2020-09-25 17:04:22 +0900

    #01 MemberService생성 -sk-

--- gittest/src/com/git/service/MemberService.java
+++ gittest/src/com/git/service/MemberService.java
@@ -1,6 +1,13 @@
 package com.git.service;
 
+import java.lang.reflect.Member;
+import java.sql.Connection;
+
 public class MemberService {
 
-	
+	public int insertMember(Member m) {
+		Connection conn;
+		int result=0;
+		return result;
+	}
 }
